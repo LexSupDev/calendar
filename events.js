6b4856834fca100d0ee721a090a1b971e75ca64f
Aleksei <lexsupdev@gmail.com>
2023-02-15 18:36:50 +0500

    Add event function

--- events.js
+++ events.js
@@ -1,29 +1,36 @@
 const eventsDateElem = document.querySelector(".events__date");
-const eventAddButton = document.querySelector(".events__addButton");
-const eventAddPopup = document.querySelector(".events__addPopup");
-const eventListObj = {
-  Events: [
-    {
-      title: "Соревнование по лёгкой атлетике",
-      date: "14.02.2023",
-      startTime: "14:30",
-      duration: "4",
-      location: "Бауманская",
-      participants: ["Тренер", "Мартышка"],
-    },
-    {
-      title: "Воллейбол",
-      date: "14.02.2023",
-      startTime: "12:30",
-      duration: "1",
-      location: "Сокольники",
-      participants: ["Саня", "Яна"],
-    },
-  ],
-};
+const eventAddButtonElem = document.querySelector(".events__addButton");
+const eventAddPopupElem = document.querySelector(".events__addPopup");
+let eventListObj = {};
+
+if (localStorage.getItem('eventsList')) {
+  eventListObj = JSON.parse(localStorage.getItem('eventsList'));
+} else {
+  eventListObj = {
+    Events: [
+      {
+        title: "Соревнование по лёгкой атлетике",
+        date: "14.02.2023",
+        startTime: "14:30",
+        duration: "4",
+        location: "Бауманская",
+        participants: ["Тренер", "Мартышка"],
+      },
+      {
+        title: "Воллейбол",
+        date: "14.02.2023",
+        startTime: "12:30",
+        duration: "1",
+        location: "Сокольники",
+        participants: ["Саня", "Яна"],
+      },
+    ],
+  };
+}
 
 function renderEvents() {
   const eventsElem = document.querySelector(".events__list");
+  eventsElem.innerHTML = '';
 
   for (let i = 0; i < eventListObj.Events.length; i++) {
     let event = document.createElement("article");
@@ -34,7 +41,7 @@ function renderEvents() {
       <div class="eventItem__row">                                                                                                
       <span class="eventItem__startTime">Время начала: <span class="eventItem__startTimeSpan">${
         eventListObj.Events[i].startTime
-      }</span>                      
+      }</span></span>                      
       <span class="eventItem__duration">\ Длительность: <span class="eventItem__durationSpan">${
         eventListObj.Events[i].duration
       }</span></span>              
@@ -45,7 +52,6 @@ function renderEvents() {
       <p class="eventItem__participants">Участники: <span class="eventItem__participantsSpan">${eventListObj.Events[
         i
       ].participants.join(",")}</span></p>`;
-
     eventsElem.appendChild(event);
   }
 
@@ -59,8 +65,32 @@ eventsDateElem.innerHTML = currentDate.toLocaleString("ru-ru", {
   day: "numeric",
 });
 
-eventAddButton.addEventListener("click", () => {
-  eventAddPopup.classList.toggle("collapse");
+eventAddButtonElem.addEventListener("click", () => {
+  eventAddPopupElem.classList.toggle("collapse");
+});
+
+const eventAddPopupFormElem = document.querySelector(".addEventPopup__form");
+
+eventAddPopupFormElem.addEventListener("submit", (e) => {
+  const eventTitleElem = document.querySelector('#event-title');
+  const eventDateElem = document.querySelector('#event-date');
+  const eventStartTimeElem = document.querySelector('#event-startTime');
+  const eventDurationElem = document.querySelector('#event-duration');
+  const eventLocationElem = document.querySelector('#event-location');
+  const eventParticipantsElem = document.querySelector('#event-participants');
+  e.preventDefault();
+
+  eventListObj.Events.push({
+    title: eventTitleElem.value,
+    date: eventDateElem.value,
+    startTime: eventStartTimeElem.value,
+    duration: eventDurationElem.value,
+    location: eventLocationElem.value,
+    participants: eventParticipantsElem.value.split(','),
+  });
+  renderEvents();
+
+  //console.log(eventListObj)
 });
 
 export { eventsDateElem };
